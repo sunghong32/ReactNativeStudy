18e9737d0d09f616ede1fcc5e2aaa76a4d506d1a
sunghong32 <sunghong32@uangel.com>
2025-05-21 20:28:24 +0900

    [feat] Expo Setup

---
+++ Ch2ProjectSetupExpo/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { WebView } from "react-native-webview";
+
+const App = () => {
+  return (
+    <WebView
+      source={{ html: "<h1>Hello WebView!</h1>" }}
+      originWhitelist={["*"]}
+    />
+  );
+};
+
+export default App;
